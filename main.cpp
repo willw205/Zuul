385849f641011d130b0f1da9a0517d51f912adc5
William Wong <williamwong@Williams-MacBook-Pro.local>
2022-12-13 17:12:06 -0800

    Included more #include and added new room (lab)

--- main.cpp
+++ main.cpp
@@ -2,9 +2,16 @@
  #include <vector>
  #include <cstring>
  #include <iostream>
+ #include <map>
+ #include <iterator>
+ #include "room.h"
  using namespace std;
 
  int main() {
+
+   room* lab = new room();
+   //lab->setName((char*)"1-20");
+   
 
  }
 
