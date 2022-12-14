bcda2057087c8be65f24565ae3ebadaf06ab8696
William Wong <williamwong@Williams-MacBook-Pro.local>
2022-12-13 17:11:30 -0800

    Creating function prototypes and variables

---
+++ room.h
@@ -0,0 +1,22 @@
+ #include <iostream>
+ #include <cstring>
+ #include <vector>
+ #include <map>
+ #include <iterator>
+
+ #ifndef ROOM_H
+ #define ROOM_H
+
+ using namespace std;
+
+ class Room{
+  public:
+
+   char* getName();
+   void setName(char* addName);
+   
+
+   
+ };
+ #endif
+
