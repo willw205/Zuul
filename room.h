e0fe4a7ab4bd39f7c2f0fb615396406ccc399595
William Wong <williamwong@Williams-MacBook-Pro.local>
2022-12-16 23:38:50 -0800

    FINAL

--- room.h
+++ room.h
@@ -6,22 +6,28 @@
 
  #ifndef ROOM_H
  #define ROOM_H
-
  using namespace std;
+
+ //Below we create function prototypes
 
  class room{
   public:
 
    char* getName();
    void setName(char* addName);
+   char* getDescription();
+   void setDescription(char* newDescription);
    vector<char*>* getItems();
    void addItem(char* itemName);
    map<const char*, room*>* getMap();
    void addExit(const char* direction, room* newRoom);
 
+  //private variables added below
+   
   private:
    
    map<const char*, room*> exits;
+   char description[100];
    char name[50];
    vector<char*> items;
  };
